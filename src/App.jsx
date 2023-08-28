367c607b6f48c5a87dd05bdd19e6df7aab5ac5b4
Tryphon Ksydas <tryphon.ksydas@gmail.com>
2023-08-28 23:26:49 +0300

    refactor: tidy up styling a bit

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,13 @@ function App() {
 
 	return (
 		<React.Fragment>
-			<span className='position-absolute text-light' style={{ top: '.25vh', right: '.5vh', zIndex: 1 }} >v1.0.0</span>
 			<Container fluid className="hero_area">
 				<header className="header_section">
 					<Container fluid>
 						<Navbar expand='lg' className='custom_nav-container' >
 							<Navbar.Brand href='/'>
 								<span>
-                Tuning Calculator <small className='fw-light fs-6 text-secondary'>Car Mechanic Simulator 21</small>
+									Tuning Calculator <small className='fw-light fs-6 text-secondary text-sm-show d-none d-sm-inline'>Car Mechanic Simulator 21</small><small className='fw-light fs-6 text-secondary text-sm-show d-inline d-sm-none'>CMS 21</small>
 								</span>
 							</Navbar.Brand>
 							<Navbar.Toggle type='button' aria-controls="navbarSupportedContent" aria-label="Toggle navigation" />

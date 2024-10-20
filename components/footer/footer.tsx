72cd5275f722fed0f1c96dc3208bc4f03165847e
Tryphon Ksydas <tryphon.ksydas@gmail.com>
2024-10-20 12:50:38 +0300

    chore: bump version

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { FaGithub } from 'react-icons/fa6';
 
 export default function Footer() {
+	console.log(Intl.DateTimeFormat('en-US').formatToParts(Date.now()));
 	return (
 		<footer className="footer items-center p-4 bg-neutral text-neutral-content">
 			<aside className="items-center grid-flow-col">
@@ -24,16 +25,22 @@ export default function Footer() {
 					Copyright © {new Date().getFullYear()} - All right reserved
 				</p>
 			</aside>
-			<nav className="flex max-md:w-full max-md:justify-center max-md:items-center md:place-self-center md:justify-self-end">
-				<Link
-					href="https://github.com/TryphonX/CMS-Tuning-Calculator/issues/new"
-					target="_blank"
-				>
-					<button className="btn btn-primary btn-sm">
-						<FaGithub aria-hidden /> Open an issue
-					</button>
-				</Link>
-			</nav>
+			<div className="flex flex-row max-md:w-full max-md:justify-center items-center md:place-self-center md:justify-self-end">
+				<p>
+					<span className="text-primary font-semibold">v2.1.0</span> |
+					October 20, 2024
+				</p>
+				<nav className="flex max-md:w-full max-md:justify-center max-md:items-center md:place-self-center md:justify-self-end">
+					<Link
+						href="https://github.com/TryphonX/CMS-Tuning-Calculator/issues/new"
+						target="_blank"
+					>
+						<button className="btn btn-primary btn-sm">
+							<FaGithub aria-hidden /> Open an issue
+						</button>
+					</Link>
+				</nav>
+			</div>
 		</footer>
 	);
 }

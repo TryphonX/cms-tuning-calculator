e737ddf59e56380dc1f78f8bd286054cbeffb7d0
Tryphon Ksydas <tryphon.ksydas@gmail.com>
2024-11-01 23:05:25 +0200

    chore: bump version

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -26,8 +26,8 @@ export default function Footer() {
 			</aside>
 			<div className="flex flex-row max-md:w-full max-md:justify-center items-center md:place-self-center md:justify-self-end">
 				<p>
-					<span className="text-primary font-semibold">v2.1.1</span> |
-					October 20, 2024
+					<span className="text-primary font-semibold">v2.1.2</span> |
+					November 02, 2024
 				</p>
 				<nav className="flex max-md:w-full max-md:justify-center max-md:items-center md:place-self-center md:justify-self-end">
 					<Link

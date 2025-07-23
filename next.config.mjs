42a1334c3e724f9f97ce80db5542c987afc62b80
Tryphon Ksydas <tryphon.ksydas@gmail.com>
2025-07-23 03:47:24 +0300

    ci: attempt to fix deploy pipeline

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,30 @@
-import packagejson from './package.json' with { type: 'json' };
 import { execSync } from 'child_process';
+import { readFileSync } from 'fs';
+
+const packagejson = JSON.parse(readFileSync('./package.json', 'utf8'));
 
 const getIsMainBranch = () => {
-  try {
-    return execSync('git rev-parse --abbrev-ref HEAD', { encoding: 'utf8' }).trim() === 'main';
-  } catch (error) {
-    console.warn('Could not determine Git branch, defaulting to "main"');
-    return true;
-  }
+	try {
+		return (
+			execSync('git rev-parse --abbrev-ref HEAD', {
+				encoding: 'utf8',
+			}).trim() === 'main'
+		);
+	} catch (error) {
+		console.warn('Could not determine Git branch, defaulting to "main"');
+		return true;
+	}
 };
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	output: 'export',
 	basePath: '/cms-tuning-calculator',
 	env: {
 		APP_VERSION: packagejson.version,
-		LAST_PUBLISH: getIsMainBranch() ? process.env.LAST_PUBLISH : new Date().toISOString(),
+		LAST_PUBLISH: getIsMainBranch()
+			? process.env.LAST_PUBLISH
+			: new Date().toISOString(),
 	},
 	images: {
 		remotePatterns: [
